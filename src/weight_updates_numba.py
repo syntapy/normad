Fixed bug I introduced in synaptic scaling a while back

src/weight_updates_numba.py
@@ -270,8 +270,8 @@ def tempotron_update_output_weights(info):
     #pudb.set_trace()
     Wo, d_Wo = info.Wo, info.d_Wo
 
-    pudb.set_trace()
-    lam = 0.2
+    #pudb.set_trace()
+    lam = 1.0
     for j in range(o):
         j_max = np.argmax(v[j])
         t_max = j_max * dt
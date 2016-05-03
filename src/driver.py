7825e2345dc371f7def1932005f0b50c99c7cfbd
Sami Abdul-Wahid <sipho.1989@gmail.com>
2016-05-03 12:15:03 -0700

    Final touches made for presentability

--- src/driver.py
+++ src/driver.py
@@ -11,8 +11,8 @@ import brian2 as br
 # Uncomment any of the following lines to chose which benchmark to solve
 
 # needs 3 inputs and at least 2 outputs
-linsep = dt.data('linsep')
-X, Y = linsep.X['train'], linsep.Y['train']
+#linsep = dt.data('linsep')
+#X, Y = linsep.X['train'], linsep.Y['train']
 
 # needs 5 inputs and at least 3 outputs
 #iris = dt.data('iris')
@@ -22,7 +22,7 @@ X, Y = linsep.X['train'], linsep.Y['train']
 xor = dt.data('xor')
 X, Y = xor.X['train'], xor.Y['train']
 
-nn = lif.net(inputs=3, hidden=40, output=3, subc=10, delay=10)
-p = nn.fit(X, Y, method_o='tempotron', method_h='tempotron', goal_accuracy=1.0)
+nn = lif.net(inputs=3, hidden=12, output=3, subc=10, delay=10)
+p = nn.fit(X, Y, method_o='tempotron', method_h='tempotron', goal_accuracy=1.0, read_weights=False)
 
 print "Test Performance:", p
